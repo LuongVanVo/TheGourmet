f575ccea93240bcea55742d06e66b9558b1c93a0
vanvo299 <luongvanvo29@gmail.com>
2026-01-09 18:05:28 +0700

    feat: implement complete features about cart: clear item, remove item, update quantity in cart

---
+++ src/TheGourmet.Application/Features/Carts/Commands/RemoveItemFromCart/RemoveItemFromCartCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TheGourmet.Application.DTOs.Cart;
+
+namespace TheGourmet.Application.Features.Carts.Commands.RemoveItemFromCart;
+
+public class RemoveItemFromCartCommand : IRequest<CartDto>
+{
+    public Guid UserId { get; set; }
+    public Guid ProductId { get; set; }
+}
---
+++ src/TheGourmet.Application/Features/Carts/Commands/UpdateQuantityProductInCart/UpdateQuantityProductInCartCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using TheGourmet.Application.DTOs.Cart;
+
+namespace TheGourmet.Application.Features.Carts.Commands.UpdateQuantityProductInCart;
+
+public class UpdateQuantityProductInCartCommand : IRequest<CartDto>
+{
+    public Guid UserId { get; set; }
+    public Guid ProductId { get; set; }
+    public int NewQuantity { get; set; }
+}
